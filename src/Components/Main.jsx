40fd89d909c62fe4b4f8f04ad8d65ebf89fcbeb4
Ankushk-01 <ankushkumar.me24@gmail.com>
2023-08-02 18:24:43 +0530

    Update Main.jsx

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,5 +1,5 @@
 import "./Style.css";
-import React from "react";
+import React, { useEffect } from "react";
 import Card from "./Card";
 import Pokeinfo from "./Pokeinfo";
 import axios from "axios";
@@ -8,6 +8,36 @@ const Main =()=>{
     const [pokeData,setPokeData] = useState([]);
     const [loading,setLoading] = useState(true);
     const [url,setUrl] = useState("https://pokeapi.co/api/v2/pokemon/");
+
+    const [nextUrl,setNextUrl] = useState();
+    const [priviousUrl,setPriviousUrl] = useState();
+
+
+    async function pokeFun(){
+        setLoading(true);
+        const res = await axios.get(url);
+        console.log(res.data.results);
+        setNextUrl(res.data.next);
+        setPriviousUrl(res.data.previous);
+        console.log(res.data.next);
+        console.log(res.data.previous);
+        getPokemon(res.data.results);
+        console.log("pokedata"+pokeData);
+        setLoading(false);
+    }
+    async function getPokemon(res){
+        res.map(async(item)=>{
+            const result = await axios.get(item.url);
+            console.log(result.data);
+            setPokeData(state=>{
+                state = [...state,result.data]
+                return state;
+            })
+        })
+    }
+    useEffect(()=>{
+        pokeFun();
+    },[url])
     return(
         <>
         <div className="container">
